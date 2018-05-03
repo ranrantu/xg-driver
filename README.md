a29f02e6839662f650c22ff73ed9460a0cc744cc
蒋翔 <jiangxiang@meituan.com>
2018-05-03 20:00:30 +0800

    add webpack4 base configeration

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ xg-driver a cli tool with dev-server and release commands
 ## recent task
 1.dev-server
 2.release
+
+Usage:
--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
+/*
+* 📦 out of the box React application development tools, built-in css-modules, babel, postcss, HMR, etc.
+* 🐠 create-react-app experience
+* 🚨 webpack configuration in JSON format
+* 🔥 mock
+* ✂️ test based on jest, ui test with enzyme
+* */
+
 const chalk = require('chalk');
 const program = require('commander');
 const Spinner = require('cli-spinner').Spinner;
 const info = require('./info.js');
 const log = console.log;
+const co = require('co');
+const prompt = require('co-prompt');
 
 
-var spinner = new Spinner('processing.. %s');
+var spinner = new Spinner('');
 spinner.setSpinnerString('|/-\\');
 
 program
     .option('-V, --version', 'print version')
+    .option('-b, --build', 'build package')
+    .option('init', 'init project')
     .parse(process.argv);
 
-if (program.pineapple) console.log(info.version);
-
 var obj = new Spinner({
-    text: 'processing.. %s',
+    text: '📦 Loading... %s',
     stream: process.stderr,
     onTick: function (msg) {
         this.clearLine(this.stream);
@@ -23,19 +33,20 @@ var obj = new Spinner({
     }
 })
 
-obj.start();
-
-setTimeout(() => {
-    obj.stop();
-}, 1000);
-
-function timeStamp(){
-    let now = new Date().getTime();
-    spinner.start();
-    let timer = new Date().getTime() - now;
-    while (timer < 2000) {
-        
-    }
+if (program.build) {
+    obj.start();
+    setTimeout(() => {
+        obj.stop();
+    }, 3000);
 }
-// timeStamp();
 
+if(program.init){
+    co(function *() {
+        var name = yield prompt('👶username: ');
+        var pass = yield prompt.password('password: ');
+        var desc = yield prompt.multiline('description: ');
+        var ok = yield prompt.confirm('are you sure? ');
+    }).then(function (){
+        console.log('finish');
+    });
+}
